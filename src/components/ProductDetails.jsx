30744d8aed95c964d6c9ebabd563ed166c7fe9de
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-05-25 14:16:45 +0600

    product dettails

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,28 +1,80 @@
 import BreadCrumb from "./Common/BreadCrumb";
 const ProductDetails = () => {
   // ------------------ slick slider
-  
+
   return (
     <>
       <section id="productDetails" className="pt-[80px]">
-        <BreadCrumb pageName={'Products Details'} />
+        <BreadCrumb pageName={"Products Details"} />
         <div className="container">
           {/* ------------- product font view */}
           <div className="flex flex-wrap gap-[60px]  mt-20 lg:pl-[200px]">
-                  <div className="w-[300px] h-[300px] bg-gray-100 overflow-hidden"><img src="sdf" alt="productImage" /></div>
-                      <div className="productText">
-                          <h2 className="text-2xl font-medium text-black">Product Name</h2>
-                          <h3 className="text-lg font-semibold text-primary mt-5">Price : <span className="font-normal">800$</span></h3>
-                          <h3 className="text-lg font-semibold text-primary mt-5">Size : <span className="font-normal">w-30m x h-10m</span></h3>
-                          <h3 className="text-lg font-semibold text-primary mt-5">Colors :</h3>
-                          <div className='flex items-center gap-3 mt-5'>
-                              <button className="w-[20px] h-[20px] bg-blue-500 rounded-full relative"></button>
-                              <button className="w-[20px] h-[20px] bg-orange-500 rounded-full relative"></button>
-                              <button className="w-[20px] h-[20px] bg-green-500 rounded-full relative"></button>
-                          </div>
-                          <button className="w-[150px] h-[35px] bg-black text-[16px] font-normal text-white mt-5 rounded-lg active:scale-[1.1]">Add to cart</button>
-                </div>
-          </div>
+            <div className="w-[300px] h-[300px] bg-gray-100 overflow-hidden">
+              <img src="sdf" alt="productImage" />
+            </div>
+            <div className="productText">
+              <h2 className="text-2xl font-medium text-black">Product Name</h2>
+              <h3 className="text-lg font-semibold text-primary mt-5">
+                Price : <span className="font-normal">800$</span>
+              </h3>
+              <h3 className="text-lg font-semibold text-primary mt-5">
+                Size : <span className="font-normal">w-30m x h-10m</span>
+              </h3>
+              <h3 className="text-lg font-semibold text-primary mt-5">
+                Colors :
+              </h3>
+              <div className="flex items-center gap-3 mt-5">
+                <button className="w-[20px] h-[20px] bg-blue-500 rounded-full relative"></button>
+                <button className="w-[20px] h-[20px] bg-orange-500 rounded-full relative"></button>
+                <button className="w-[20px] h-[20px] bg-green-500 rounded-full relative"></button>
+              </div>
+              <button className="w-[150px] h-[35px] bg-black text-[16px] font-normal text-white mt-5 rounded-lg active:scale-[1.1]">
+                Add to cart
+              </button>
+            </div>
+                  </div>
+                  <div className="  my-10  ">
+                      {/* ---------------- singel product title  */}
+                      <div className="flex gap-10 flex-wrap py-5 border-b border-gray-200">
+                          <h2 className=" w-[250px] text-lg  font-poppins  font-medium text-primary">Material</h2>
+                          <p className="text-[16px] font-poppins font-normal text-pretty "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, saepe.</p>
+                      </div>
+                      {/* ---------------- singel product title  */}
+                      <div className="flex gap-10 flex-wrap py-5 border-b border-gray-200">
+                          <h2 className=" w-[250px] text-lg  font-poppins  font-medium text-primary">Delivery Information</h2>
+                          <p className="text-[16px] font-poppins font-normal text-pretty "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, saepe.</p>
+                      </div>
+                      {/* ---------------- singel product title  */}
+                      <div className="flex gap-10 flex-wrap py-5 border-b border-gray-200">
+                          <h2 className=" w-[250px] text-lg  font-poppins  font-medium text-primary">Key Features & Highlights</h2>
+                          <p className="text-[16px] font-poppins font-normal text-pretty "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, saepe.</p>
+                      </div>
+                      {/* ---------------- singel product title  */}
+                      <div className="flex gap-10 flex-wrap py-5 border-b border-gray-200">
+                          <h2 className=" w-[250px] text-lg  font-poppins  font-medium text-primary">Specifications </h2>
+                          <p className="text-[16px] font-poppins font-normal text-pretty "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, saepe.</p>
+                      </div>
+                      {/* ---------------- singel product title  */}
+                      <div className="flex gap-10 flex-wrap py-5 border-b border-gray-200">
+                          <h2 className=" w-[250px] text-lg  font-poppins  font-medium text-primary">Care Instructions</h2>
+                          <p className="text-[16px] font-poppins font-normal text-pretty "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, saepe.</p>
+                      </div>
+                      {/* ---------------- singel product title  */}
+                      <div className="flex gap-10 flex-wrap py-5 border-b border-gray-200">
+                          <h2 className=" w-[250px] text-lg  font-poppins  font-medium text-primary">Estimated Delivery Date</h2>
+                          <p className="text-[16px] font-poppins font-normal text-pretty "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, saepe.</p>
+                      </div>
+                      {/* ---------------- singel product title  */}
+                      <div className="flex gap-10 flex-wrap py-5 border-b border-gray-200">
+                          <h2 className=" w-[250px] text-lg  font-poppins  font-medium text-primary">Return & Warranty Policy</h2>
+                          <p className="text-[16px] font-poppins font-normal text-pretty "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, saepe.</p>
+                      </div>
+                      {/* ---------------- singel product title  */}
+                      <div className="flex gap-10 flex-wrap py-5 border-b border-gray-200">
+                          <h2 className=" w-[250px] text-lg  font-poppins  font-medium text-primary">Shipping & Assembly Information</h2>
+                          <p className="text-[16px] font-poppins font-normal text-pretty "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, saepe.</p>
+                      </div>
+                  </div>
         </div>
       </section>
     </>
